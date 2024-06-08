5a4446cefa64bec86abb71432d541c61336da5de
alejohh0404 <alejohh0404@gmail.com>
2024-06-08 23:54:23 +0000

    salto de linea

--- EDA-Project/src/eda/project/EDAProject.java
+++ EDA-Project/src/eda/project/EDAProject.java
@@ -15,7 +15,7 @@ public class EDAProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print('Hola mundo');
+        System.out.println('Hola mundo');
     }
     
 }
